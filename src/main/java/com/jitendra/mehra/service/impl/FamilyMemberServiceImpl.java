78efddd5ea42cf7205e13eadb866ba2b838eb89c
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-02-10 19:37:49 +0530

    CR218 commit

--- src/main/java/com/jitendra/mehra/service/impl/FamilyMemberServiceImpl.java
+++ src/main/java/com/jitendra/mehra/service/impl/FamilyMemberServiceImpl.java
@@ -2,6 +2,8 @@ package com.jitendra.mehra.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import com.jitendra.mehra.service.FamilyMemberService;
 
 @Service
 public class FamilyMemberServiceImpl implements FamilyMemberService {
+	
+	private final Logger logger = LoggerFactory.getLogger(FamilyMemberServiceImpl.class);
 	
 	@Autowired
 	private FamilyMemberRepository repository;
