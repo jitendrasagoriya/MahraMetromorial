3dc42afe1463c04586b32ec5b9a5a69678d19a25
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-02-10 13:17:35 +0530

    First Commit

---
+++ src/main/java/com/jitendra/mehra/MahraMetromorial/MahraMetromorialApplication.java
@@ -0,0 +1,12 @@
+package com.jitendra.mehra.MahraMetromorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahraMetromorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahraMetromorialApplication.class, args);
+	}
+}
