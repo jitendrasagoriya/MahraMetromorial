6be16ce10654ae7b325cd1e5879e81ec1d91ad5a
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-02-21 11:43:19 +0530

    Added log

--- src/main/java/com/jitendra/mehra/service/impl/FacebookConnectionSignup.java
+++ src/main/java/com/jitendra/mehra/service/impl/FacebookConnectionSignup.java
@@ -2,6 +2,8 @@ package com.jitendra.mehra.service.impl;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
@@ -13,12 +15,15 @@ import com.jitendra.mehra.repository.UserRepository;
 @Service
 public class FacebookConnectionSignup implements ConnectionSignUp {
 	
+	private final Logger logger = LoggerFactory.getLogger(FacebookConnectionSignup.class);
+	
+	
 	 @Autowired
 	 private UserRepository userRepository;
 
 	@Override
 	public String execute(Connection<?> connection) {
-		System.out.println("signup === ");
+		logger.info("signup {} ",connection);
         final User user = new User();
         user.setUsername(connection.getDisplayName());
         user.setPassword(randomAlphabetic(8));
