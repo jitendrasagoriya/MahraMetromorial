407f489acc48e7ee357e3456db9f7ca830029ad4
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-05-16 09:50:39 +0530

    Added PUT request in CrosFilter.java

--- src/main/java/com/jitendra/mehra/filter/CrosFilter.java
+++ src/main/java/com/jitendra/mehra/filter/CrosFilter.java
@@ -25,7 +25,7 @@ public class CrosFilter extends  OncePerRequestFilter  {
 		   
 		   
 	        response.setHeader("Access-Control-Allow-Origin", "*");
-	        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+	        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 	        response.setHeader("Access-Control-Max-Age", "3600");
 	        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token , x-requested-with, authorization");
 	       
